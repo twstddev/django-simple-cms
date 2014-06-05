96ba3c938024acd97f0e8ede92358d0d5c9265d0
twstddev <twstd.dev@gmail.com>
2014-06-05 15:59:43 +0000

    Add slug prepopulating on page creation

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pages.models import Page
 
-# Register your models here.
+class PageAdmin( admin.ModelAdmin ):
+	prepopulated_fields = { "slug" : ( "title", ) }
+
+admin.site.register( Page, PageAdmin )
--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Page( models.Model ):
+	"""
+	Implements a simple static page.
+	"""
 	title = models.CharField( max_length = 255 )
 	slug = models.SlugField( max_length = 255 )
 	body = models.TextField()
