bd4744c614d43061420eff662a15595f70256678
twstddev <twstd.dev@gmail.com>
2014-06-05 16:05:55 +0000

    Add tinymce

--- pages/admin.py
+++ pages/admin.py
@@ -4,4 +4,10 @@ from pages.models import Page
 class PageAdmin( admin.ModelAdmin ):
 	prepopulated_fields = { "slug" : ( "title", ) }
 
+	class Media:
+		js = [
+			'/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
+			'/static/grappelli/tinymce_setup/tinymce_setup.js',
+		]
+
 admin.site.register( Page, PageAdmin )
