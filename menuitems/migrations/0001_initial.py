919fde041cc148a552c17f2e0df806d2fc49f103
twstddev <twstd.dev@gmail.com>
2014-06-05 11:06:57 +0000

    Add menu item model and migration

---
+++ menuitems/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'MenuItem'
+        db.create_table(u'menuitems_menuitem', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal(u'menuitems', ['MenuItem'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'MenuItem'
+        db.delete_table(u'menuitems_menuitem')
+
+
+    models = {
+        u'menuitems.menuitem': {
+            'Meta': {'object_name': 'MenuItem'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['menuitems']
--- menuitems/tests.py
+++ menuitems/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+class MenuItemTestCase( TestCase ):
+	"""
+	Tests menu items model.
+	"""
+	def setUp( self ):
+		pass
+
+	def test_requieres_title_to_be_set( self ):
+		"""
+		Makes sure that title is required.
+		"""
+		pass
+
+	def test_requires_url_to_be_set( self ):
+		"""
+		Makes sure that url is required.
+		"""
+		pass
+
+	def test_works_as_a_tree( self ):
+		"""
+		Checks if menu items can act as a tree and be nested.
+		"""
+		pass
